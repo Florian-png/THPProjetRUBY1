5b37071d403537f5b8f89ec0a3c45b6fc4c6b7cb
Florian-png <florian.chauvain@gmail.com>
2020-04-07 19:41:16 +0200

    changement taille

--- exo_19.rb
+++ exo_19.rb
@@ -9,6 +9,7 @@ end
 
 y = -1 # on déclare une varialbe y qui aura -1 au commencement 
 # Puis on affiche uniquement ceux qui sont paire 
+
 mails.each do  # Ici on prend tous les mails dans une boucle
     y += 1 # On ajoute 1 à Y pour qu'il commence à 0 et récupère le premier élément (et +1 à chauqe tour de boucle)
      if y.even? # On verifie si y est paire
