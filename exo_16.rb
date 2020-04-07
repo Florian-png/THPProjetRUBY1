e70d01e7461221909a0ff113cfde313f0e680218
Florian-png <florian.chauvain@gmail.com>
2020-04-07 16:18:11 +0200

    envoie

--- exo_16.rb
+++ exo_16.rb
@@ -2,15 +2,16 @@
 #Écris un programme exo_16.rb qui va demander son âge à l'utilisateur, et qui, pour chaque année depuis sa naissance, 
 #dira "Il y a X ans, tu avais Y ans".
 
-puts("Bonjour, pouvez vous me donner votre année de naissance ?")
+puts("Bonjour, pouvez vous me donner votre âge ?")
 print ">"
-user_year = gets.chomp.to_i
+
+current_age = gets.chomp.to_i 
 current_year = 2020
-user_age = 0 
+birth_date = current_year - current_age 
+birth = 0
 
-
-    while user_year != current_year
-        puts("Il y a #{current_year-user_year} ans, tu avais #{user_age} ans")
-        user_age += 1
-        user_year += 1 
+    while birth_date != current_year 
+        puts("Il y a #{current_year - birth_date} ans, tu avais #{birth} ans")
+        birth_date += 1
+        birth +=1
     end
